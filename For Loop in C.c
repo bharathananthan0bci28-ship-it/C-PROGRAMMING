fda321c5c4aea59380354ef74e8a7a283d79cd64
bharathananthan0bci28-ship-it <bharathananthan0_bci28@mepcoeng.ac.in>
2026-08-05 16:26:27 +0530

    Create For Loop in C.c

---
+++ For Loop in C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+
+    const char *digits[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int n = a; n <= b; n++) {
+        if (n >= 1 && n <= 9) {
+            printf("%s\n", digits[n - 1]);
+        } else if (n % 2 == 0) {
+            printf("even\n");
+        } else {
+            printf("odd\n");
+        }
+    }
+
+    return 0;
+}
