4e91b3283665112e3dfc9cca2ea98895e58bb466
bharathananthan0bci28-ship-it <bharathananthan0_bci28@mepcoeng.ac.in>
2026-08-05 16:24:19 +0530

    Create Functions in C.c

---
+++ Functions in C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d) {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d\n%d\n%d\n%d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d\n", ans);
+    return 0;
+}
